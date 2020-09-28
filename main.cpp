1abe620c82bdc6f1cefccb260b5ccc0cb5265ef9
Rafal Cardona <rafael.cardona@alumnos.udg.mx>
2020-09-28 10:40:21 -0500

    Codigo area de un cuadrado

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    int lado;
+    cout << "Ingresa el lado del cuadrado: ";
+    cin >> lado;
+    cout << "El area del cuadrado es: "<<lado*lado;
+    
+    getch();
     return 0;
 }
